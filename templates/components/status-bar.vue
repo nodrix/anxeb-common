21bed685f206c3138bb71e630684db747ce39820
Eduardo Rosario <erosario@nodrix.com>
2021-02-24 12:20:42 -0400

    Status bar improvements

--- templates/components/status-bar.vue
+++ templates/components/status-bar.vue
@@ -1,4 +1,4 @@
-<div>
+<div class="app-box-shadow">
 <div class="text-xs p-2">
   <div class="pull-right" :class="{'text-muted': options == null || options.muted !== false}">
     <span v-if="$slots.caption !== null"><slot name="caption"></slot></span>
