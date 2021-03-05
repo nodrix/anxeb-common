54ff07722dd996c21708207d19c474d9a5910783
Eduardo Rosario <erosario@nodrix.com>
2021-03-05 08:50:50 -0400

    Allow Actions component to be shown as a context menu

--- includes/components/actions.js
+++ includes/components/actions.js
@@ -2,11 +2,20 @@
 
 anxeb.vue.include.component('actions', function (helpers) {
 	return {
-		props    : ['title', 'icon', 'layout', 'enabled', 'active'],
+		props    : ['title', 'icon', 'layout', 'enabled', 'active', 'offset'],
 		template : '/components/actions.vue',
 		mounted  : function () {
 			let _self = this;
 			$(document).bind('mouseup.' + _self.name, function (e) {
+
+				if (e.button === 2 && _self.contextElement === e.target) {
+					_self.toggle(true, {
+						x : e.originalEvent.clientX,
+						y : e.originalEvent.offsetY
+					});
+					return;
+				}
+
 				let box = $(_self.$refs.box);
 				let button = $(_self.$refs.btn);
 				if (button.is(e.target) || button.has(e.target).length > 0 || box.is(e.target) || box.has(e.target).length > 0) {
@@ -17,22 +26,46 @@ anxeb.vue.include.component('actions', function (helpers) {
 			});
 		},
 		methods  : {
-			toggle : function (visibility) {
+			contextmenu : function (e) {
+				this.contextElement = e.target;
+			},
+			toggle      : function (visibility, position) {
+				let $visibility;
 				if (this.enabled !== false) {
 					this.$emit('click');
 					if (visibility != null) {
-						this.visible = visibility;
+						$visibility = visibility;
 					} else {
-						this.visible = !this.visible;
+						$visibility = !this.visible;
 					}
+				}
+
+				if ($visibility && position) {
+					this.position = position;
+				} else {
+					this.position = null;
+				}
+				this.visible = $visibility;
+			}
+		},
+		computed : {
+			positionStyle : function () {
+				if (this.position) {
+					return {
+						top  : `${this.position.y + (this.offset.y || 0)}px`,
+						left : `${this.position.x - (this.offset.x || 0)}px`
+					}
+				} else {
+					return null;
 				}
 			}
 		},
 		data     : function () {
 			return {
-				visible : false
+				contextElement : null,
+				visible        : false,
+				position       : null
 			}
-		},
-		computed : {}
+		}
 	}
 });
--- templates/components/actions.vue
+++ templates/components/actions.vue
@@ -1,23 +1,23 @@
 <div class="_group app-actions-button" v-if="layout === 'toolbar'" :class="{'app-disabled' : enabled === false}">
-	<button ref="btn" v-on:click.stop="toggle()">
-		<i class="fas" :class="[icon, (active === true ? 'app-terminal-active-color' : null)]"></i><span>${title}</span>
-	</button>
-
-	<div ref="box" class="_actions" v-show="visible" v-if="$slots.options !== undefined">
-		<div class="dropdown-menu" v-on:click="toggle(false)">
-			<slot name="options"></slot>
-		</div>
-	</div>
+  <button ref="btn" v-on:click.stop="toggle()">
+    <i class="fas" :class="[icon, (active === true ? 'app-terminal-active-color' : null)]"></i><span>${title}</span>
+  </button>
+  
+  <div ref="box" class="_actions" v-show="visible" v-if="$slots.options !== undefined" :style="positionStyle">
+    <div class="dropdown-menu" v-on:click="toggle(false)">
+      <slot name="options"></slot>
+    </div>
+  </div>
 </div>
 <div v-else class="app-actions-container">
-	<div class="app-actions-button">
-		<button ref="btn" v-on:click.stop="toggle()">
-			${title}
-		</button>
-		<div ref="box" class="_actions" v-show="visible" v-if="$slots.options !== undefined">
-			<div class="dropdown-menu" v-on:click="toggle(false)">
-				<slot name="options"></slot>
-			</div>
-		</div>
-	</div>
+  <div class="app-actions-button">
+    <button ref="btn" v-on:click.stop="toggle()">
+      ${title}
+    </button>
+    <div ref="box" class="_actions" v-show="visible" v-if="$slots.options !== undefined">
+      <div class="dropdown-menu" v-on:click="toggle(false)">
+        <slot name="options"></slot>
+      </div>
+    </div>
+  </div>
 </div>
