ba6c9a15f335a32190b118003242cd74999289b3
Eduardo Rosario <erosario@nodrix.com>
2021-03-05 16:01:14 -0400

    Slug converter prototype

--- static/scripts/global.js
+++ static/scripts/global.js
@@ -4,20 +4,20 @@
 
 	FastClick.attach(document.body);
 
-	if ( !!navigator.userAgent.match(/MSIE/i) || !!navigator.userAgent.match(/Trident.*rv:11\./) ){
+	if (!!navigator.userAgent.match(/MSIE/i) || !!navigator.userAgent.match(/Trident.*rv:11\./)) {
 		$('body').addClass('ie');
 	}
 
 	let ua = window['navigator']['userAgent'] || window['navigator']['vendor'] || window['opera'];
-	if( (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua) ){
+	if ((/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua)) {
 		$('body').addClass('smart');
 	}
 
-	$('input, textarea').each(function(){
+	$('input, textarea').each(function () {
 		$(this).val() ? $(this).addClass('has-value') : $(this).removeClass('has-value');
 	});
 
-	$(document).on('blur', 'input, textarea', function(e){
+	$(document).on('blur', 'input, textarea', function (e) {
 		$(this).val() ? $(this).addClass('has-value') : $(this).removeClass('has-value');
 	});
 
@@ -26,7 +26,7 @@
 		$this.is('a') || ($this = $this.closest('a'));
 
 		$li = $this.parent();
-		$active = $li.siblings( ".active" );
+		$active = $li.siblings(".active");
 		$li.toggleClass('active');
 		$active.removeClass('active');
 	});
@@ -42,17 +42,28 @@ function enableTouch(e, el) {
 	return true;
 }
 
-String.prototype.toCamelCase = function() {
-	if (this == null){
+String.prototype.toCamelCase = function () {
+	if (this == null) {
 		return null;
 	}
-	if (this.length === 1){
+	if (this.length === 1) {
 		return this.toUpperCase();
 	}
 	let result = this
-		.replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+		.replace(/\s(.)/g, function ($1) { return $1.toUpperCase(); })
 		.replace(/\s/g, '')
-		.replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+		.replace(/^(.)/, function ($1) { return $1.toLowerCase(); });
 
 	return result[0].toUpperCase() + result.substr(1);
 }
+
+String.prototype.toSlug = function (separator = "-") {
+	return this
+		.toString()
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.toLowerCase()
+		.trim()
+		.replace(/[^a-z0-9 ]/g, '')
+		.replace(/\s+/g, separator);
+};
