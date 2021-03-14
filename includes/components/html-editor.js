bec3c15a5be08e040ed20c399c7c68d9f6e6104d
Eduardo Rosario <erosario@nodrix.com>
2021-03-14 10:42:26 -0400

    HTML editor component

---
+++ includes/components/html-editor.js
@@ -0,0 +1,84 @@
+'use strict';
+
+anxeb.vue.include.component('html-editor', function (helpers) {
+	let _editor;
+
+	return {
+		template  : '/components/html-editor.vue',
+		props     : ['value', 'height', 'gallery-url'],
+		inject    : ['page', 'log', 'modal', 'dialogs'],
+		created   : function () {
+			this.html = this.value;
+			this.sync();
+		},
+		updated   : function () {
+			this.html = this.value;
+			this.sync();
+		},
+		destroyed : function () {
+			if (_editor) {
+				_editor.destroy();
+				_editor = null;
+			}
+		},
+		mounted   : function () {
+			let _self = this;
+
+			if (_editor == null) {
+				let textarea = $(_self.$refs.textarea).get(0);
+				_editor = window.SUNEDITOR.create(textarea, {
+					height           : 'auto',
+					value            : _self.html || '',
+					fullScreenOffset : 300,
+					popupDisplay     : 'local',
+					iframe           : true,
+					imageFileInput   : true,
+					videoFileInput   : false,
+					audioFileInput   : false,
+					imageGalleryUrl  : _self.galleryUrl,
+					lang             : SUNEDITOR_LANG['es'],
+					buttonList       : [
+						['bold', 'underline', 'italic', 'strike'],
+						['font', 'fontSize', 'formatBlock'],
+						['fontColor', 'hiliteColor', 'textStyle'],
+						['removeFormat', 'paragraphStyle', 'blockquote'],
+						['outdent', 'indent'],
+						['align', 'horizontalRule', 'list', 'lineHeight'],
+						_self.galleryUrl != null ? ['table', 'link', 'imageGallery', 'image', 'video', 'audio', 'codeView'] : ['table', 'link', 'image', 'video', 'audio', 'codeView']
+					]
+				});
+
+				_editor.onChange = function (e, core) {
+					_self.html = _editor.getContents();
+					_self.$emit('input', _self.html);
+				};
+			}
+		},
+		methods   : {
+			sync : function () {
+				if (_editor != null) {
+					let $value = this.html || '';
+					let current = _editor.getContents();
+
+					if (current !== $value) {
+						_editor.setContents($value);
+					}
+				}
+			}
+		},
+		watch     : {
+			value : function (value) {
+				if (this.html !== value) {
+					this.html = value;
+					this.sync();
+				}
+			}
+		},
+		data      : function () {
+			return {
+				html : null
+			}
+		}
+	}
+})
+;
---
+++ templates/components/html-editor.vue
@@ -0,0 +1,3 @@
+<div>
+  <textarea style="width: 100%"  ref="textarea"></textarea>
+</div>
