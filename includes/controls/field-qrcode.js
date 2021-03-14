a6042791fd7627ad401b06b8fc6e0ef49dd3f70a
Eduardo Rosario <erosario@nodrix.com>
2021-03-14 17:46:55 -0400

    QR code field

---
+++ includes/controls/field-qrcode.js
@@ -0,0 +1,52 @@
+'use strict';
+
+anxeb.vue.include.component('field-qrcode', function (helpers) {
+	let _qr;
+
+	return {
+		template     : '/controls/field-qrcode.vue',
+		inheritAttrs : false,
+		props        : ['label', 'value', 'id', 'readonly', 'height', 'width', 'field-name', 'max-height', 'max-width', 'size', 'display-value'],
+		mounted      : function () {
+			let _self = this;
+			this.name = _self.fieldName || (_self.$vnode.data.model != null ? _self.$vnode.data.model.expression : null);
+			this.render();
+		},
+		data         : function () {
+			return {
+				name : null,
+				code : null
+			}
+		},
+		watch        : {
+			value : function () {
+				this.render();
+			}
+		},
+		methods      : {
+			render : function () {
+				let _self = this;
+				let container = _self.$refs.container;
+				let value = _self.value;
+
+				let $size = _self.size || 200;
+				if (_self.width > $size) {
+					$size = _self.width;
+				}
+				if (_self.height > $size) {
+					$size = _self.height;
+				}
+
+				_qr = new QRious({
+					element : container,
+					size    : $size,
+					value   : value
+				});
+			},
+			edit   : function () {
+				this.$emit('click');
+			}
+		},
+		computed     : {}
+	};
+});
---
+++ templates/controls/field-qrcode.vue
@@ -0,0 +1,22 @@
+<div>
+	<div v-bind="$attrs" v-if="label == null || label.length === 0">
+		<div :field-name="name" v-if="readonly == null || readonly === false" v-on:click="edit()" :id="id" style="cursor: pointer!important">
+			<canvas ref="container" src="" :style="{width: width, height:height, 'max-width': maxWidth, 'max-height':maxHeight}" />
+		</div>
+		<div :field-name="name" v-else :id="id" style="cursor: default!important">
+			<canvas ref="container" src="" :style="{width: width, height:height, 'max-width': maxWidth, 'max-height':maxHeight}" />
+		</div>
+	</div>
+	<div v-bind="$attrs" class="app-field-parent" v-if="label != null && label.length > 0">
+		<div v-bind:class="{'md-form-group' : label != null}" class="app-field-input-container" :field-name="name">
+			<div v-if="readonly == null || readonly === false" class="app-field-barcode-container" v-on:click="edit()" :id="id">
+				<canvas ref="container" src="" :style="{width: width, height:height, 'max-width': maxWidth, 'max-height':maxHeight}" />
+			</div>
+			<div v-else class="app-field-barcode-container" :id="id" style="cursor: default!important">
+				<canvas ref="container" src="" :style="{width: width, height:height, 'max-width': maxWidth, 'max-height':maxHeight}" />
+			</div>
+			<input class="md-input" style="display: none">
+			<label>${label}</label>
+		</div>
+	</div>
+</div>
