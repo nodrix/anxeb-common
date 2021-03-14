05efe07064a91475d82ebdda61742110f833476b
Eduardo Rosario <erosario@nodrix.com>
2021-03-14 10:43:54 -0400

    Field input minor improvements

--- templates/controls/field-input.vue
+++ templates/controls/field-input.vue
@@ -1,24 +1,24 @@
 <div v-bind="$attrs" class="app-field-parent">
-	<div class="md-form-group app-field-input-container" :field-name="name" v-if="type === 'calendar'">
-		<vc-date-picker ref="calendar" mode='single' title-position="left" locale="es" v-model="date" :popover="{placement : calendarPlacement || 'top-end', visibility: 'focus'}" :is-expanded="false">
-			<input v-fill-focus="autoSelect === 'true' || autoSelect === true" style="cursor: pointer" :class="{'text-right':align==='right'}" :style="{color:valueColor, 'font-weight': valueWeight}" :readonly="true" type="text" class="md-input" :value="post_value" v-on="listener" :id="id" :required="required !== undefined" v-focus="focus === undefined ? false : focus">
-		</vc-date-picker>
-		<span style="position: relative; left: -10px; top:-2px"><i class="fa fa-times _action" style="color:black" v-on:click="clear" v-if="date != null"></i></span>
-		<span style="position: relative; left: -10px; top:-2px"><i class="fa fa-calendar _action" style="color:black; pointer-events: none;" v-if="date == null"></i></span>
-
-		<input type="hidden" class="md-input">
-		<label style="width: 100%" :class="{'app-field-label-text-right':align==='right'}">
-			${label}
-		</label>
-	</div>
-
-	<div class="md-form-group app-field-input-container" :field-name="name" v-if="type !== 'calendar'">
-		<input v-fill-focus="autoSelect === 'true' || autoSelect === true" :class="{'text-right':align==='right'}" :style="{color:valueColor, 'font-weight': valueWeight}" :readonly="readonly === 'true' || readonly === true" v-if="type !== 'area'" :type="type || 'text'" step="any" v-bind:autocomplete="type === 'password'" class="md-input" :value="post_value" v-on="listener" :id="id" :required="required !== undefined" v-focus="focus === undefined ? false : focus" :maxlength="maxLength">
-		<textarea :class="{'text-right':align==='right'}" :style="{height: height, color:valueColor, 'font-weight': valueWeight}" :readonly="readonly === 'true' || readonly === true" v-if="type === 'area'" class="md-input" :value="value" v-on="listener" :id="id" :required="required !== undefined" v-focus="focus === undefined ? false : focus" :rows="rows" :maxlength="maxLength"></textarea>
-
-		<label style="width: 100%" :class="{'app-field-label-right':align==='right'}">
-			${label}
-			<span v-show="readonly !== 'true' && readonly !== true" class="pull-right text-color" :class="{'text-danger' : value != null && (value.length > maxLength || value.length < (minLength || 0))}" v-if="maxLength !== null && maxLength > 0">${value != null ? value.length : 0} / ${minLength != null ? (minLength + '-') : ''}${maxLength} dígitos</span>
-		</label>
-	</div>
+  <div class="md-form-group app-field-input-container" :field-name="name" v-if="type === 'calendar'">
+    <vc-date-picker ref="calendar" mode="single" title-position="left" locale="es" v-model="date" :popover="{placement : calendarPlacement || 'top-end', visibility: 'focus'}" :is-expanded="false">
+      <input v-fill-focus="autoSelect === 'true' || autoSelect === true" style="cursor: pointer" :class="{'text-right':align==='right'}" :style="{color:valueColor, 'font-weight': valueWeight}" :readonly="true" type="text" class="md-input" :value="post_value" v-on="listener" :id="id" :required="required !== undefined" v-focus="focus === undefined ? false : focus">
+    </vc-date-picker>
+    <span style="position: relative; left: -10px; top:-2px"><i class="fa fa-times _action" style="color:black" v-on:click="clear" v-if="date != null"></i></span>
+    <span style="position: relative; left: -10px; top:-2px"><i class="fa fa-calendar _action" style="color:black; pointer-events: none;" v-if="date == null"></i></span>
+    
+    <input type="hidden" class="md-input">
+    <label v-if="label != null" style="width: 100%" :class="{'app-field-label-text-right':align==='right'}">
+      ${label}
+    </label>
+  </div>
+  
+  <div class="md-form-group app-field-input-container" :field-name="name" v-if="type !== 'calendar'">
+    <input v-fill-focus="autoSelect === 'true' || autoSelect === true" :class="{'text-right':align==='right'}" :style="{color:valueColor, 'font-weight': valueWeight}" :readonly="readonly === 'true' || readonly === true" v-if="type !== 'area'" :type="type || 'text'" step="any" v-bind:autocomplete="type === 'password'" class="md-input" :value="post_value" v-on="listener" :id="id" :required="required !== undefined" v-focus="focus === undefined ? false : focus" :maxlength="maxLength">
+    <textarea :class="{'text-right':align==='right'}" :style="{height: height, color:valueColor, 'font-weight': valueWeight}" :readonly="readonly === 'true' || readonly === true" v-if="type === 'area'" class="md-input" :value="value" v-on="listener" :id="id" :required="required !== undefined" v-focus="focus === undefined ? false : focus" :rows="rows" :maxlength="maxLength"></textarea>
+    
+    <label v-if="label != null" style="width: 100%" :class="{'app-field-label-right':align==='right'}">
+      ${label}
+      <span v-show="readonly !== 'true' && readonly !== true" class="pull-right text-color" :class="{'text-danger' : value != null && (value.length > maxLength || value.length < (minLength || 0))}" v-if="maxLength !== null && maxLength > 0">${value != null ? value.length : 0} / ${minLength != null ? (minLength + '-') : ''}${maxLength} dígitos</span>
+    </label>
+  </div>
 </div>
