b4e3c0814445f11a7d9423bc0eeddc13ea1fe7dd
Eduardo Rosario <erosario@nodrix.com>
2021-03-14 19:36:22 -0400

    Header groping parameter, minor code refactoring

--- includes/directives/context-menu.js
+++ includes/directives/context-menu.js
@@ -1,47 +1,36 @@
 'use strict';
 
-anxeb.vue.include.directive('context-menu', {
-	bind     : function (el, directive) {
-		if (directive.value != null && directive.value.disabled !== true) {
-			el.oncontextmenu = async function (e) {
-				try {
-					let cmenu = await anxeb.vue.helpers.root.$fetch('context-menu');
-					if (cmenu) {
-						let $actions = directive.value instanceof Array ? directive.value : directive.value.actions;
-						let $options = directive.value instanceof Array ? null : directive.value.options;
+anxeb.vue.include.directive('context-menu', function (helpers) {
 
-						cmenu.pop({
-							event   : e,
-							actions : $actions,
-							options : $options
-						});
-					}
-				} catch (err) { }
-				e.preventDefault();
-			}
+	let getContextHandler = function (el, params) {
+		return async function (e) {
+			try {
+				let cmenu = await anxeb.vue.helpers.root.$fetch('context-menu');
+				if (cmenu) {
+					let $actions = params instanceof Array ? params : params.actions;
+					let $options = params instanceof Array ? null : params.options;
+
+					cmenu.pop({
+						event   : e,
+						actions : $actions,
+						options : $options
+					});
+				}
+			} catch (err) { }
+			e.preventDefault();
 		}
-	},
-	update   : function (el, directive) {
-		if (directive.value != null && directive.value.disabled !== true) {
-			el.oncontextmenu = async function (e) {
-				try {
-					let cmenu = await anxeb.vue.helpers.root.$fetch('context-menu');
-					if (cmenu) {
-						let $actions = directive.value instanceof Array ? directive.value : directive.value.actions;
-						let $options = directive.value instanceof Array ? null : directive.value.options;
-
-						cmenu.pop({
-							event   : e,
-							actions : $actions,
-							options : $options
-						});
-					}
-				} catch (err) { }
-				e.preventDefault();
-			}
-		}
-	},
-	inserted : function (el) {
-
 	}
+
+	return {
+		bind   : function (el, directive) {
+			if (directive.value != null && directive.value.disabled !== true) {
+				el.oncontextmenu = getContextHandler(el, directive.value);
+			}
+		},
+		update : function (el, directive) {
+			if (directive.value != null && directive.value.disabled !== true) {
+				el.oncontextmenu = getContextHandler(el, directive.value);
+			}
+		}
+	};
 });
--- templates/components/context-menu.vue
+++ templates/components/context-menu.vue
@@ -2,7 +2,8 @@
   <div class="_actions">
     <div class="dropdown-menu">
       <div class="_container" v-if="context != null">
-        <div v-for="item in context.actions" v-disabled="item.isDisabled != null && item.isDisabled() === true">
+        <div v-for="item in context.actions" v-show="item.isVisible == null || item.isVisible() !== false" v-disabled="item.isDisabled != null && item.isDisabled() === true">
+          <div class="_header" v-if="item.header != null">${item.header}</div>
           <div class="_divisor" v-if="item.divider"></div>
           <a class="_label" :class="item.class" v-on:click.stop="execute(item)">
             <i v-if="item.icon != null" class="_icon fa" :class="item.icon"></i> ${item.caption}
