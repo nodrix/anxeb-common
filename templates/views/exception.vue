2934158cc27c751768450f2dd8bacfd8b1e26125
Eduardo Rosario <erosario@nodrix.com>
2021-03-20 18:01:03 -0400

    Including calendar stylesheet and removing unneeded common views

--- templates/views/exception.vue
+++
@@ -1,9 +0,0 @@
-<div class="app-exception-container">
-	<div class="code">
-		<span class="fas fa-exclamation-circle"></span> {{code}}
-	</div>
-	<div class="message">
-		<div class="text">{{message}}</div>
-		<a href="/">back home</a>
-	</div>
-</div>
