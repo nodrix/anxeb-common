1fc7e89e39f4b3a105c749365aa85057fdaa6660
Eduardo Rosario <erosario@nodrix.com>
2021-03-25 17:26:40 -0400

    Allow field maps to persist zoom; new radius circle attribute

--- includes/controls/field-maps.js
+++ includes/controls/field-maps.js
@@ -1,11 +1,10 @@
 'use strict';
 
 anxeb.vue.include.component('field-maps', function (helpers) {
-
 	return {
 		template     : '/controls/field-maps.vue',
 		inheritAttrs : false,
-		props        : ['label', 'id', 'readonly', 'height', 'width', 'field-name', 'size', 'init-address', 'caption', 'value', 'maps', 'address', 'zoom', 'options'],
+		props        : ['label', 'id', 'readonly', 'height', 'width', 'field-name', 'size', 'init-address', 'caption', 'value', 'maps', 'address', 'zoom', 'options', 'circle'],
 		created      : async function () {
 
 		},
@@ -18,7 +17,7 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 			}
 		},
 		methods      : {
-			init        : function () {
+			init         : function () {
 				let _self = this;
 				_self.map = new _self.maps.Map(_self.$refs.container, {
 					scaleControl     : !_self.isReadonly,
@@ -32,6 +31,12 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 				if (!_self.isReadonly) {
 					_self.map.controls[_self.maps.ControlPosition.TOP_LEFT].push(_self.$refs.cbutton);
 
+					_self.map.addListener('zoom_changed', function () {
+						if (_self.location) {
+							_self.location.zoom = _self.map.getZoom();
+						}
+					});
+
 					_self.map.addListener('center_changed', function () {
 
 					});
@@ -44,21 +49,21 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 						}
 					});
 					_self.map.addListener('click', function (mapsMouseEvent) {
-						_self.location = mapsMouseEvent.latLng;
+						_self.location = _self.parseCoords(mapsMouseEvent.latLng);
 					});
 				}
 			},
-			refresh     : async function () {
+			refresh      : async function () {
 				let _self = this;
 				_self.lock = true;
 				if (_self.value == null) {
 					_self.geometry = _self.geometry || (await _self.getLocation(_self.initAddress || 'Dominican Republic'));
 					if (_self.location == null) {
 						_self.map.fitBounds(_self.geometry.viewport);
-						_self.location = _self.geometry.location;
+						_self.location = _self.parseCoords(_self.geometry.location);
 					}
 				} else {
-					_self.location = _self.value;
+					_self.location = _self.parseCoords(_self.value, _self.value.zoom);
 				}
 
 				if (_self.marker == null) {
@@ -70,7 +75,7 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 					});
 
 					_self.marker.addListener('dragend', function () {
-						_self.location = _self.marker.getPosition();
+						_self.location = _self.parseCoords(_self.marker.getPosition());
 					})
 				}
 
@@ -82,7 +87,7 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 				}, 200)
 
 			},
-			getLocation : function (address) {
+			getLocation  : function (address) {
 				let _self = this;
 				return new Promise(function (resolve, reject) {
 					_self.geocoder = _self.geocoder || (new _self.maps.Geocoder());
@@ -108,23 +113,29 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 					getGeometry();
 				});
 			},
-			bringMarker : function () {
-				this.location = this.map.getCenter();
+			bringMarker  : function () {
+				this.location = this.parseCoords(this.map.getCenter());
 				this.updateMap();
 			},
-			gotoMarker  : function () {
+			gotoMarker   : function () {
 				this.updateMap();
 			},
-			updateMap   : function (params) {
+			updateMap    : function (params) {
 				let value = params != null ? params.value : null;
 				let _self = this;
 				if (_self.marker && _self.map) {
 					if (value || _self.value) {
-						_self.marker.setPosition(value || _self.value);
+						let $position = value || _self.value;
+						_self.marker.setPosition($position);
+						_self.renderCircle();
+
 						setTimeout(function () {
-							if (_self.zoomed === false) {
-								_self.map.setZoom((_self.options != null ? _self.options.zoom : null) || 15);
-								_self.zoomed = true;
+							if (_self.location.zoom || _self.zoomed === false) {
+								let $zoom = _self.location.zoom || (_self.options != null ? _self.options.zoom : null) || 15;
+								if ($zoom) {
+									_self.map.setZoom($zoom);
+									_self.zoomed = true;
+								}
 							} else if (_self.options != null && _self.options.zoom != null) {
 								_self.map.setZoom(_self.options.zoom);
 							}
@@ -132,16 +143,59 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 						}, 10);
 					}
 				}
+			},
+			parseCoords  : function (location, zoom) {
+				return {
+					lat  : typeof location.lat === 'function' ? location.lat() : location.lat,
+					lng  : typeof location.lng === 'function' ? location.lng() : location.lng,
+					zoom : zoom || this.map.getZoom()
+				}
+			},
+			renderCircle : function () {
+				let _self = this;
+				let $position = _self.value;
+				if (_self.circle != null && $position != null) {
+					let $radius;
+					if (isNaN(_self.circle.radius)) {
+						$radius = 0;
+					} else {
+						$radius = parseFloat(_self.circle.radius);
+					}
+
+					if (_self.$circle == null) {
+						_self.$circle = new _self.maps.Circle({
+							strokeWeight : 0,
+							fillColor    : _self.circle.color || "#fd2727",
+							fillOpacity  : _self.circle.opacity || 0.25,
+							clickable    : false,
+							map          : _self.map,
+							center       : $position,
+							radius       : $radius
+						});
+					} else {
+						_self.$circle.setRadius($radius);
+						_self.$circle.setCenter($position)
+					}
+				}
 			}
 		},
 		watch        : {
-			location : function (value) {
-				if (!this.lock) {
-					this.$emit('input', value);
-					this.updateMap({
-						value : value
-					});
-				}
+			location : {
+				handler : function (value) {
+					if (!this.lock) {
+						this.$emit('input', value);
+						this.updateMap({
+							value : value
+						});
+					}
+				},
+				deep    : true
+			},
+			circle   : {
+				handler : function (value) {
+					this.renderCircle();
+				},
+				deep    : true
 			},
 			address  : async function (value) {
 				let _self = this;
@@ -151,7 +205,7 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 
 					if (_self.geometry && _self.geometry.location) {
 						_self.map.fitBounds(_self.geometry.viewport);
-						_self.location = _self.geometry.location;
+						_self.location = _self.parseCoords(_self.geometry.location);
 
 						if (_self.marker == null) {
 							_self.marker = new _self.maps.Marker({
@@ -162,7 +216,7 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 							});
 
 							_self.marker.addListener('dragend', function () {
-								_self.location = _self.marker.getPosition();
+								_self.location = _self.parseCoords(_self.marker.getPosition());
 							})
 						} else {
 							_self.marker.setPosition(_self.location);
@@ -186,6 +240,7 @@ anxeb.vue.include.component('field-maps', function (helpers) {
 		},
 		data         : function () {
 			return {
+				$circle  : null,
 				lock     : null,
 				name     : null,
 				marker   : null,
